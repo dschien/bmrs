b74c9022ece73d0463977d3116cd4767b706ea42
Daniel Schien <dance@web.de>
2019-01-22 13:02:34 +0000

    fix pull client

--- push_client.py
+++ push_client.py
@@ -13,7 +13,7 @@ class MyListener(stomp.ConnectionListener):
         print('received an error "%s"' % message)
 
     def on_message(self, headers, message):
-        for k, v in headers.iteritems():
+        for k, v in headers.items():
             print('header: key %s , value %s' % (k, v))
             print('received a message "%s"' % message)
             with open("messages.log", "a") as logfile:
@@ -24,6 +24,6 @@ conn = stomp.Connection12(host_and_ports=[('api.bmreports.com', 61613)], use_ssl
 conn.set_listener('', MyListener())
 conn.start()
 conn.connect(YOUR_API_KEY_HERE, YOUR_API_KEY_HERE, True)
-conn.subscribe(destination='/topic/INDDEM', ack='auto', id='CS_UOB')
+conn.subscribe(destination='/topic/bmrsTopic', ack='auto', id='CS_UOB')
 while conn.is_connected():
     time.sleep(1)
